abbcbe1864c453befb1cce024f469312dff0c011
heckerboy <93342694+heckerboy@users.noreply.github.com>
2021-10-29 16:57:00 +0530

    Add files via upload

---
+++ Lab-6/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# import turtle library
+import turtle             
+my_pen = turtle.Turtle()      
+for i in range(4):
+   my_pen.forward(100)           
+   my_pen.right(20)               
+turtle.done()
